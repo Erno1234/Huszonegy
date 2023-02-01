4751bc85537a2f251e4d988f93aa51f9b03e69b1
Erno1234 <palocierno12>
2023-02-01 15:20:31 +0100

    Nagyobb mint 21 pont, vesztett

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 #Megoldás
-def eredmeny():
-    pass
+def eredmeny(jatekosPontjai, gepPontjai):
+    if jatekosPontjai > 21:
+        print("Játékos vesztett")
+    elif gepPontjai > 21:
+        print("Gép vesztett")
 #Tesztesetek
