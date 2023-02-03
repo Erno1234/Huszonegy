5e4521a74f5f39d0d6a1bc9d2235396af06ce5cc
Erno1234 <palocierno12>
2023-02-03 12:25:05 +0100

    sikeres teszt

--- main.py
+++ main.py
@@ -1,7 +1,33 @@
 #Megoldás
-def eredmeny(jatekosPontjai, gepPontjai):
-    if jatekosPontjai > 21:
-        print("Játékos vesztett")
-    elif gepPontjai > 21:
-        print("Gép vesztett")
+def eredmeny(jatekosLapok: [int], gepLapok: [int]):
+    szoveg = ""
+    jatekosPont = pontszamitas(jatekosLapok)
+    gepPont = pontszamitas(gepLapok)
+
+    if jatekosPont > 21:
+        szoveg = "Játékos vesztett"
+    elif gepPont > 21:
+        szoveg = "Gép vesztett"
+    return szoveg
+def pontszamitas(lapok: [int]):
+    pontok = 0
+    for i in range(len(lapok)):
+        pontok += lapok[i]
+    return pontok
+
 #Tesztesetek
+
+def jatekosVesztettTeszt():
+    jatekosLista = [6, 5, 7, 8]
+    gepLista = [6, 5, 8]
+    kapottEredmeny = eredmeny(jatekosLista, gepLista)
+    vartEredmeny = "Játékos vesztett"
+    if kapottEredmeny == vartEredmeny:
+        print("A teszt sikeres.")
+    else:
+        print("A teszt megbukott.")
+def tesztek():
+    jatekosVesztettTeszt()
+
+
+tesztek()
