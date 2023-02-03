01444257208c72611d2871b04f9f32e23e3640bb
Erno1234 <palocierno12>
2023-02-03 13:30:20 +0100

    További teszt lehetőségek

--- main.py
+++ main.py
@@ -8,10 +8,6 @@ def eredmeny(jatekosLapok: [int], gepLapok: [int]):
         szoveg = "Játékos vesztett"
     elif gepPont > 21:
         szoveg = "Gép vesztett"
-    elif gepPont > jatekosPont:
-        szoveg = "Játékos vesztett"
-    elif jatekosPont > gepPont:
-        szoveg = "Gép vesztett"
     return szoveg
 def pontszamitas(lapok: [int]):
     pontok = 0
@@ -27,9 +23,9 @@ def jatekosVesztettTeszt():
     kapottEredmeny = eredmeny(jatekosLista, gepLista)
     vartEredmeny = "Játékos vesztett"
     if kapottEredmeny == vartEredmeny:
-        print("A teszt sikeres.")
+        print("A teszt sikeres. (Játekos teszt: Pont nagyobb mint 21)")
     else:
-        print("A teszt megbukott.")
+        print("A teszt megbukott. (Játekos teszt: Pont nagyobb mint 21)")
 
 def jatekosKevesebbP():
     jatekosKev = [4, 5, 6, 3]
@@ -37,12 +33,62 @@ def jatekosKevesebbP():
     kapottEredmeny = eredmeny(jatekosKev, gepKev)
     vartEredmeny = "Játékos vesztett"
     if kapottEredmeny == vartEredmeny:
-        print("A teszt sikeres.")
+        print("A teszt sikeres. (Játékos teszt: Kevesebb pont)")
     else:
-        print("A teszt megbukott.")
+        print("A teszt megbukott. (Játékos teszt: Kevesebb pont)")
+def jatekosEgyenloP_TobbL():
+    jatekosKev = [4, 5, 6, 3, 2]
+    gepKev = [5, 5, 5, 5]
+    kapottEredmeny = eredmeny(jatekosKev, gepKev)
+    vartEredmeny = "Játékos vesztett"
+    if kapottEredmeny == vartEredmeny:
+        print("A teszt sikeres. (Játékos teszt: Egyenlő pontok több lap)")
+    else:
+        print("A teszt megbukott. (Játékos teszt: Egyenlő pontok több lap)")
+def gepNagyobb_H_egy():
+    jatekosKev = [4, 5, 6, 8]
+    gepKev = [5, 5, 5, 3]
+    kapottEredmeny = eredmeny(jatekosKev, gepKev)
+    vartEredmeny = "Gép vesztett"
+    if kapottEredmeny == vartEredmeny:
+        print("A teszt sikeres.(Gép teszt: Pont nagyobb mint 21)")
+    else:
+        print("A teszt megbukott. (Gép teszt: Pont kevesebb mint 21)")
+def gepKevesebbP():
+    jatekosKev = [4, 5, 6, 3]
+    gepKev = [5, 5, 5]
+    kapottEredmeny = eredmeny(jatekosKev, gepKev)
+    vartEredmeny = "Gép vesztett"
+    if kapottEredmeny == vartEredmeny:
+        print("A teszt sikeres.(Gép teszt: Kevesebb pont)")
+    else:
+        print("A teszt megbukott. (Gép teszt: Kevesebb pont)")
+def gepEgyenloP_TobbL():
+    jatekosKev = [4, 5, 6, 3]
+    gepKev = [5, 5, 8]
+    kapottEredmeny = eredmeny(jatekosKev, gepKev)
+    vartEredmeny = "Gép vesztett"
+    if kapottEredmeny == vartEredmeny:
+        print("A teszt sikeres.(Gép teszt: Egyenlő pontok több lap)")
+    else:
+        print("A teszt megbukott. (Gép teszt: Egyenlő pontok több lap)")
+def dontetlen():
+    jatekosLista = [4, 5, 6, 3]
+    gepLista = [5, 5, 5, 3]
+    kapottEredmeny = eredmeny(jatekosLista, gepLista)
+    vartEredmeny = "Döntetlen"
+    if kapottEredmeny == vartEredmeny:
+        print("A teszt sikeres.(Döntetlen)")
+    else:
+        print("A teszt megbukott. (Döntetlen)")
 def tesztek():
     jatekosVesztettTeszt()
     jatekosKevesebbP()
+    jatekosEgyenloP_TobbL()
+    gepNagyobb_H_egy()
+    gepKevesebbP()
+    gepEgyenloP_TobbL()
+    dontetlen()
 
 
 tesztek()
